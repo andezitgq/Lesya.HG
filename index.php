3867a8173f9ce55cfe136d4dc8af3b0fe4ee3b05
andezitgq <sanerhp.mail@gmail.com>
2020-08-21 21:19:32 +0300

    Redbean

--- index.php
+++ index.php
@@ -4,9 +4,14 @@
         $all = R::findAll('postdate',' ORDER BY date ASC LIMIT 10');
         for($i = count($all); $i >= 1; $i--){
             $date = date_create($all[$i]->date);
-            echo date_format($date, 'd.m.Y G:i')."<br>";
             $post = R::findOne('post', 'date_id = ?', array($all[$i]->id));
-            echo $post->content;
+            $author = R::findOne('postauthor', 'id = ?', array($post->author_id));
+            $title = R::findOne('posttitle', 'id = ?', array($post->title_id));
+            echo '<div class=post>
+                      <i class=post-date>'.date_format($date, 'd.m.Y G:i').' 📝 '.$author->author.'</i>
+                      <h1>'.$title->title.'</h1><hr><br>'.
+                      $post->content.
+                  '</div>';
         }
     
     ?>
