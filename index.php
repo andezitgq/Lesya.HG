6620bfda69756b37ad377b3aeade6121545287e3
andezitgq <sanerhp.mail@gmail.com>
2020-08-22 13:02:15 +0300

    Redbean

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <?php include 'tml/top.php'; ?>
     <?php
     
-        $all = R::findAll('postdate',' ORDER BY date ASC LIMIT 10');
-        for($i = count($all); $i >= 1; $i--){
-            $date = date_create($all[$i]->date);
-            $post = R::findOne('post', 'date_id = ?', array($all[$i]->id));
-            $author = R::findOne('postauthor', 'id = ?', array($post->author_id));
-            $title = R::findOne('posttitle', 'id = ?', array($post->title_id));
-            echo '<div class=post>
-                      <i class=post-date>'.date_format($date, 'd.m.Y G:i').' 📝 '.$author->author.'</i>
-                      <h1>'.$title->title.'</h1><hr><br>'.
-                      $post->content.
-                  '</div>';
+        //$all = R::findAll('postdate',' ORDER BY date DESC LIMIT 125 ');
+        $all = R::getAll( 'SELECT * FROM postdate ORDER BY date DESC LIMIT 125' );
+        for($i = -1; $i <= count($all); $i++){
+            if(isset($all[$i])){
+                $date = date_create($all[$i]['date']);
+                $post = R::findOne('post', 'date_id = ?', array($all[$i]['id']));
+                $author = R::findOne('postauthor', 'id = ?', array($post->author_id));
+                $title = R::findOne('posttitle', 'id = ?', array($post->title_id));
+                echo '<div class=post>
+                          <i class=post-date>'.date_format($date, 'd.m.Y G:i').' 📝 '.$author->author.'</i>
+                          <h1>'.$title->title.'</h1><hr><br>'.
+                          $post->content.
+                      '</div>';
+            }
         }
     
     ?>
