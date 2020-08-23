22f472cd5b376f878278143af134bc874568b8ae
andezitgq <sanerhp.mail@gmail.com>
2020-08-23 19:35:29 +0300

    Restyle

--- index.php
+++ index.php
@@ -2,18 +2,19 @@
     <div class=wrap-content-box>
     <?php
     
-        $all = R::getAll( 'SELECT * FROM postdate ORDER BY date DESC LIMIT 10' );
+        $all = R::getAll( 'SELECT * FROM postdate ORDER BY date DESC LIMIT 15' );
         for($i = -1; $i <= count($all); $i++){
             if(isset($all[$i])){
                 $date = date_create($all[$i]['date']);
                 $post = R::findOne('post', 'date_id = ?', array($all[$i]['id']));
                 $author = R::findOne('postauthor', 'id = ?', array($post->author_id));
                 $title = R::findOne('posttitle', 'id = ?', array($post->title_id));
-                echo '<div class=post>
-                          <i class=post-date>'.date_format($date, 'd.m.Y G:i').' 📝 '.$author->author.'</i>
-                          <h1>'.$title->title.'</h1><hr><br>'.
-                          $post->content.
-                      '</div>';
+                if($post->id != 88)
+                    echo '<div class=post>
+                              <i class=post-date>'.date_format($date, 'd.m.Y G:i').' 📝 '.$author->author.'</i>
+                              <h1>'.$title->title.'</h1><hr><br>'.
+                              $post->content.
+                          '</div>';
             }
         }
     
--- showpost.php
+++ showpost.php
@@ -1,10 +1,10 @@
 <?php include 'tml/top.php'; ?>
     <?php
-        if(!isset($_GET['postid']) || $_GET['postid'] == '')
+        if(!isset($_GET['postid']) || $_GET['postid'] == '' || $_GET['postid'] == 0)
             echo '<script>window.location.href = "/";</script>';
             
         $post = R::findOne('post', 'id = ?', array($_GET['postid']));
-        if($post){ 
+        if($post){
             $date = date_create(R::findOne('postdate', 'id = ?', array($post->date_id))->date);
             $author = R::findOne('postauthor', 'id = ?', array($post->author_id));
             $title = R::findOne('posttitle', 'id = ?', array($post->title_id));
