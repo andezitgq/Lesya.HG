8880b15e9f9d50691ee369b81a2c26ea5ebad389
andezitgq <sanerhp.mail@gmail.com>
2020-10-23 23:21:19 +0300

    Info page

--- info.php
+++ info.php
@@ -14,7 +14,20 @@ include 'tml/top.php';
         </div>
     </div>
     <div class="document-view">
-        document
+        <div class="lesya-pdf" style="width: 100%">
+            <div role="toolbar" id="toolbar">
+                <center><div id="pager">
+                    <p>Правила вступу до гімназії</p>
+                    <button class=icon-left-open data-pager="prev"></button>
+                    <button class=icon-right-open data-pager="next"></button>
+                    <button class=icon-download onclick="download('img/Вступ.pdf', 'Вступ до гімназії.pdf')"></button>
+                </div></center>
+                <div id="page-mode" style="display:none">
+                    <label>Page Mode <input type="number" value="1" min="1"/></label>
+                </div>
+            </div>
+            <div id="viewport-container"><div role="main" id="viewport"></div></div>
+        </div>
     </div>
 </div>
 
