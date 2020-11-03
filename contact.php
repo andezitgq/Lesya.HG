1ff4053a824c5929ed25c8e625213e2fc20cab92
andezitgq <sanerhp.mail@gmail.com>
2020-11-04 01:44:17 +0200

    Contact

--- contact.php
+++ contact.php
@@ -5,4 +5,24 @@
 
 ?>
 
+<div class="contact-main">
+    <form action="contact" class=contact-form method="post">
+        <h2 align="center">Прямий звʼязок</h2>
+
+    </form>
+    <div class="contact-info">
+        <h2 align="center">Наші контакти:</h2>
+        <center>
+            <p>52210 Україна Дніпропетровська область <br>м.Жовті Води, бульвар Свободи, 18<br>&#160 тел.:(099) 607 78 74, (096) 600 77 10,&#160,&#160<br> e-mail: <a href="mailto:lesya_zhv@ukr.net">lesya_zhv@ukr.net</a></p>
+            <br>
+            <a href="http://dnepredu.com/" target="_blank">
+                <img src="http://lesya.org/images/portal.gif" height="38">
+            </a>
+            <a href="http://goroozv.dnepredu.com/" target="_blank">
+                <img src="http://lesya.org/images/miskvo.gif" height="38">
+            </a>
+        </center>   
+    </div>
+</div>
+
 <?php include 'tml/bottom.php'; ?>
