c801893948ab2a7faa28834f3d8bc2b7496a943f
andezitgq <sanerhp.mail@gmail.com>
2020-11-04 02:03:22 +0200

    Contact

--- contact.php
+++ contact.php
@@ -5,17 +5,27 @@
 
     $data = $_POST;
 
-    
+    if(isset($data['send_mail'])){
+        $conf_subject = $data['topic'];
+            
+            $headers = "From: ".$data['name']." <".$data['email'].">\r\n";
+            $headers .= "MIME-Version: 1.0\r\n";
+            $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+            $msg = $data['message'];
+                    
+            mail('freelogger@localhost', $conf_subject, $msg, $headers);
+    }
 
 ?>
 
 <div class="contact-main">
     <form action="contact" class=contact-form method="post">
         <h2 align="center">Прямий звʼязок</h2>
-        <input type="text" placeholder="Ваше ім'я" required>
-        <input type="email" placeholder="Ваш E-mail" required>
-        <textarea name="" id="" placholder="Ваше повідомлення" style="resize: vertical" required></textarea>
-        <button type="submit">Відправити</button>
+        <input name=name id=name type="text" placeholder="Ваше ім'я" required>
+        <input name=email id=email type="email" placeholder="Ваш E-mail" required>
+        <input name=topic id=topic type="text" placeholder="Тема листа" required>
+        <textarea name="message" id="message" placeholder="Ваше повідомлення" style="resize: vertical" required></textarea>
+        <button type="submit" name=send_mail id=send_mail>Відправити</button>
     </form>
     <div class="contact-info">
         <h2 align="center">Наші контакти:</h2>
