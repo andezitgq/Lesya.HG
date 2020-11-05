57f302cc10087197cc682dbcb8eb71179378c51e
andezitgq <sanerhp.mail@gmail.com>
2020-11-05 12:30:32 +0200

    Documents

--- info.php
+++ info.php
@@ -33,16 +33,12 @@ include 'tml/top.php';
         <div class="lesya-pdf" style="width: 100%">
             <div role="toolbar" id="toolbar">
                 <center><div id="pager">
-                    <p>Правила вступу до гімназії</p>
-                    <button class=icon-left-open data-pager="prev"></button>
-                    <button class=icon-right-open data-pager="next"></button>
-                    <button class=icon-download onclick="download('img/Вступ.pdf', 'Вступ до гімназії.pdf')"></button>
+                    <p style="margin: 5px; margin-right: auto"><?php if(isset($document_name)) echo $document_name; ?></p>
                 </div></center>
                 <div id="page-mode" style="display:none">
-                    <label>Page Mode <input type="number" value="1" min="1"/></label>
                 </div>
             </div>
-            <div id="viewport-container"><div role="main" id="viewport"></div></div>
+            <div id="viewport-container"><div role="main" id="viewport"><iframe class=canvas src="http://lesya.org/nasha_gimnazia/metodychna_robota/P_F_index.html"></iframe></div></div>
         </div>
     </div>
 </div>
